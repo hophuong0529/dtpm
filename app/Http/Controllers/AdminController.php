cf0633f2bb499513d850609f8dbfbf79ee244519
phuonght-fabbi <phuonght@fabbi.io>
2020-12-02 20:31:35 +0700

    first commit

---
+++ app/Http/Controllers/AdminController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class AdminController extends Controller
+{
+    public function index() 
+    {
+        return view('index');   
+    }
+
+    public function postLogin(Request $request) 
+    {
+        $username = $request->username;
+        $password = md5($request->password);
+        $user = DB::table('account')->where('username', $username)->where('password', $password)->get();
+        if (count($user) == 0) {
+            return redirect()->back()->with('alert', 'Incorrect username or password.');
+        } else {
+            session(['user' => $username]);
+            return redirect('welcome');
+        }
+    }
+}
